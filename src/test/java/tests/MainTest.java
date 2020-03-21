869669d836416ce4436628e5305765f510afc4d9
danikmartyniuk <daniilmartyniuk.sbmt@gmail.com>
2020-03-21 23:44:18 +0300

    cleaned the code

--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -3,15 +3,11 @@ package tests;
 import models.JobSeeker;
 import org.testng.annotations.Test;
 
-import java.util.Scanner;
-
 public class MainTest extends BaseTest {
 
     @Test
     public void findJob() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Your number");
-        int number = s.nextInt();
+
         JobSeeker user = new JobSeeker("QA Automation", "500", "1", "1", "1 4", "0 3", "2");
 
         mainSteps.findJob();
